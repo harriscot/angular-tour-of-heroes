6b8ab5da5d493c049dff9ef7661678822818484c
Neil Harris <cycling.cobol@gmail.com>
2020-08-25 19:30:01 +0100

    added routing but detail section requires a fix

---
+++ src/app/drivers.ts
@@ -0,0 +1,14 @@
+import {Hero} from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Nigel Mansell'},
+    { id: 12, name: 'Ayrton Senna'},
+    { id: 13, name: 'Michael Schumacher'},
+    { id: 14, name: 'Alain Prost'},
+    { id: 15, name: 'Lewis Hamilton'},
+    { id: 16, name: 'Jenson Button'},
+    { id: 15, name: 'David Coulthard'},
+    { id: 16, name: 'Jackie Stewart'},
+    { id: 17, name: 'Max Verstappen'},
+    { id: 18, name: 'Valtteri Bottas'},
+]
